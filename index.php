23cc94f1e93aed6134bfb1d3d0a4cdef1c32a712
tora87 <t.hanasaka.sys19@morijyobi.ac.jp>
2020-06-19 11:28:27 +0900

    new index.php

--- index.php
+++ index.php
@@ -1,32 +1,34 @@
 <?php
-    $dsn = 'mysql:dbname=sample_db;host=localhost;';
-    $user = 'toranosuke';
-    $password = 'LFO:Nirvash0';
-    try {
-        $dbh = new PDO($dsn, $user, $password);
-
-        $sql = "select * from sample;";
-        $result = $dbh->query($sql);
-
-    } catch (PDOException $e) {
-        print "Failed connect: " . $e->getMessage() . "\n";
-        exit();
-    }
-    
-    
+$dsn = 'mysql:dbname=sample_db;host=localhost;';
+$user = 'toranosuke';
+$password = 'LFO:Nirvash0';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+​
+    $sql = "select * from user;";
+    $result = $dbh->query($sql);
+} catch (PDOException $e) {
+    print "Failed Connect: " . $e->getMessage() . "\n";
+    exit();
+}
+​
 ?>
-
+​
 <!DOCTYPE html>
 <html lang="ja">
+​
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LAMP TestPage</title>
+    <title>LAMP TEST PAGE</title>
 </head>
+​
 <body>
-    <?php foreach($result as $value ){ ?>
-        <h1><?php echo "$value[name]<br>";?></h1>
-    <?php}?>
-    
+    <div class="container">
+        <?php foreach ($result as $value) { ?>
+            <?php echo "$value[name]"; ?>
+        <?php } ?>
+    </div>
 </body>
+​
 </html>
